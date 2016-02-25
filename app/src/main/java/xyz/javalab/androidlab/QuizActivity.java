cd7432448c12e85afb70925b144b664b78f7df0d
Thiago Porciúncula <tfcporciuncula@gmail.com>
2016-02-25 19:04:20 -0300

    Done with chapter 2

--- app/src/main/java/xyz/javalab/androidlab/QuizActivity.java
+++ app/src/main/java/xyz/javalab/androidlab/QuizActivity.java
@@ -4,23 +4,50 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button btnTrue;
     private Button btnFalse;
+    private ImageButton btnNext;
+    private ImageButton btnPrevious;
+    private TextView txtQuestion;
+
+    private int currentIndex = 0;
+    private Question[] questions = {
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_oceans, true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        btnTrue  = (Button) findViewById(R.id.button_true);
+        setUpWidgets();
+        updateQuestion();
+    }
+
+    private void setUpWidgets() {
+        txtQuestion = (TextView) findViewById(R.id.text_question);
+        txtQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNextQuestion();
+            }
+        });
+
+        btnTrue = (Button) findViewById(R.id.button_true);
         btnTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -28,9 +55,51 @@ public class QuizActivity extends AppCompatActivity {
         btnFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        btnNext = (ImageButton) findViewById(R.id.button_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNextQuestion();
+            }
+        });
+        btnPrevious = (ImageButton) findViewById(R.id.button_previous);
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPreviousQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        txtQuestion.setText(questions[currentIndex].getTextId());
+    }
+
+    private void showNextQuestion() {
+        currentIndex = (currentIndex + 1) % questions.length;
+        updateQuestion();
+    }
+
+    private void showPreviousQuestion() {
+        currentIndex = currentIndex - 1;
+        if (currentIndex < 0) {
+            currentIndex = questions.length - 1;
+        }
+        updateQuestion();
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        int messageId;
+        if (userPressedTrue == questions[currentIndex].isAnswerTrue()) {
+            messageId = R.string.correct_toast;
+        } else {
+            messageId = R.string.incorrect_toast;
+        }
+        Toast.makeText(QuizActivity.this, messageId, Toast.LENGTH_SHORT).show();
     }
 
 }
