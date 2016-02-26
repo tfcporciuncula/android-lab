96bbb60940b0f9bdb374a6a21e8f772602278c24
Thiago Porciúncula <tfcporciuncula@gmail.com>
2016-02-26 20:38:40 -0300

    Starting chapter 7

---
+++ app/src/main/java/xyz/javalab/androidlab/criminalintent/activities/CrimeActivity.java
@@ -0,0 +1,21 @@
+package xyz.javalab.androidlab.criminalintent.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import xyz.javalab.androidlab.R;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+
+}
---
+++ app/src/main/java/xyz/javalab/androidlab/criminalintent/model/Crime.java
@@ -0,0 +1,31 @@
+package xyz.javalab.androidlab.criminalintent.model;
+
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID id;
+    private String title;
+
+    public Crime() {
+        id = UUID.randomUUID();
+    }
+
+    public Crime(String title) {
+        this();
+        this.title = title;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
