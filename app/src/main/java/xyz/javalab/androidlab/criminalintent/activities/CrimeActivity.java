1f7d790711fc3181a1f32fceb79a3e47c09dec67
Thiago Porciúncula <tfcporciuncula@gmail.com>
2016-02-26 22:09:23 -0300

    Done with chapter 7

--- app/src/main/java/xyz/javalab/androidlab/criminalintent/activities/CrimeActivity.java
+++ app/src/main/java/xyz/javalab/androidlab/criminalintent/activities/CrimeActivity.java
@@ -1,14 +1,12 @@
 package xyz.javalab.androidlab.criminalintent.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v4.app.FragmentManager;
 
 import xyz.javalab.androidlab.R;
+import xyz.javalab.androidlab.criminalintent.fragments.CrimeFragment;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -16,6 +14,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
 }
---
+++ app/src/main/java/xyz/javalab/androidlab/criminalintent/fragments/CrimeFragment.java
@@ -0,0 +1,49 @@
+package xyz.javalab.androidlab.criminalintent.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import xyz.javalab.androidlab.R;
+import xyz.javalab.androidlab.criminalintent.model.Crime;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime crime;
+
+    private EditText titleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        crime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        titleField = (EditText) view.findViewById(R.id.crime_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                crime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        return view;
+    }
+}
